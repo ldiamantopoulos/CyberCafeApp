f3fe014fdd564ebcbc0d261f952d58b00b939420
ldiamantopoulos <ldiamantopoulos@sqlearn.com>
2021-05-19 14:55:17 +0300

    updates

---
+++ errors.php
@@ -0,0 +1,7 @@
+<?php
+//if(count($errors) > 0) :
+//    ?>
+<!--<div>-->
+<!---->
+<!--</div>-->
+<?php //endif; ?>
--- server.php
+++ server.php
@@ -27,7 +27,7 @@ if (empty($username)) {
     array_push($errors, "Password is required");
 } elseif (empty($password_2)) {
     array_push($errors, "Repeat password is required");
-}elseif ($password != $password_2){
+} elseif ($password != $password_2) {
     array_push($errors, "Password must be same as repeated password");
 }
 
@@ -36,15 +36,48 @@ $user_check_query = "Select * FROM user WHERE username = '$username' or email = 
 $results = mysqli_query($db, $user_check_query);
 $user = mysqli_fetch_assoc($results);
 
-if ($user){
-    if($user['username'] === $username) {
+if ($user) {
+    if ($user['username'] === $username) {
         array_push($errors, "Username already exists");
-    }elseif ($user['email'] === $email){
+    } elseif ($user['email'] === $email) {
         array_push($errors, "Email already exists");
     }
 }
 
-if(count($errors) == 0){
+if (count($errors) == 0) {
     $password = md5($password);//this encrypts the password
-    $query
+    $query = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$password')";
+
+    mysqli_query($db, $query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are now logged in";
+
+    header('location: index1.php');
+
+}
+
+//login user
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (empty($username)) {
+        array_push($errors, 'empty');
+    } elseif (empty($password)) {
+        array_push($errors, 'empty');
+    }
+    if (count($errors) == 0) {
+        $password = md5($password);
+
+        $query = "SELECT * FROM user WHERE username='$username' and password = '$password'";
+        $results = mysqli_query($db, $query);
+
+        if (mysqli_num_rows($results)) {
+            $_SESSION['username'] == $username;
+            $_SESSION['success'] == "Logged succesfully";
+            header('location: index1.php');
+        } else {
+            array_push($errors, "Wrong whatever");
+        }
+    }
 }
